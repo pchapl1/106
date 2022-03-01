3d5c6380383ae3db0c642764d2ae0b107732a815
Phil Chaplin <pchapl1@gmail.com>
2022-02-28 20:55:16 -0700

    added session3

---
+++ task_manager_2/scripts/task.js
@@ -0,0 +1,11 @@
+class Task{
+    constructor(title, important, dueDate, contact, location, description, color){
+        this.title = title;
+        this.important = important;
+        this.dueDate = dueDate;
+        this.contact = contact;
+        this.location = location;
+        this.description = description;
+        this.color = color;
+    }
+}
